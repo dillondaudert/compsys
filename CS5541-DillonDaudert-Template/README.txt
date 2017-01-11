259ade2840eeff22080d5150bc56b4b670d9349d
Dillon Daudert <dillongdaudert@gmail.com>
2017-01-11 15:45:31 -0500

    Create template assignment directory

---
+++ CS5541-DillonDaudert-Template/README.txt
@@ -0,0 +1,4 @@
+Date: 
+Class: CS 5541
+Assignment: 
+Author(s): Dillon Daudert
---
+++ CS5541-DillonDaudert-Template/test.c
@@ -0,0 +1,14 @@
+/*
+ * Date:
+ * Class: CS 5541
+ * Assignment: 
+ * Author(s): Dillon Daudert
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hello\n");
+	return 1;
+}
